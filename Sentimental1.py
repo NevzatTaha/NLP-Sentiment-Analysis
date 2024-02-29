de72471e53c896480dd5357d70bd4cedd80a5164
Nevzat Taha <nevtahaayhan@gmail.com>
2024-02-29 13:26:17 +0300

    Machine Learning started

--- Sentimental1.py
+++ Sentimental1.py
@@ -7,7 +7,6 @@ import re
 df=pd.read_csv('test.csv',encoding='latin1')
 
 df_=df.copy()
-
 df
 
 df.info()
@@ -18,15 +17,13 @@ df.isna().sum();
 
 df=df.dropna(how='all',axis=0);
 ### Some Rows are completely null.
-import re
-
 
 
 def text_clean(text):
     text = re.sub(pattern=r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',repl= '',string= text)  # For the mail
     text = re.sub(pattern=r'\b(?:https?://)\S+\b', repl= '',string= text)  # For the URL
     text = re.sub(pattern=r'\bhttps?://\S+\b', repl= '',string= text)  # For the URL
-    text = re.sub(pattern=r'[-/()_\']', repl= '',string= text)  
+    text = re.sub(pattern=r'[-/_\']', repl= '',string= text)  
     text = re.sub(pattern='[0-9]', repl= '',string= text)   # For removing numbers
     return(text) 
  
@@ -35,12 +32,21 @@ df["text"]=df["text"].apply(text_clean)
 
 df["text"]=df["text"].str.strip()### For the white space
 
+###  For the all same country, population, land are and density have to be same
 
+country_mistake=[]
+for country in df.columns:
+    if df[df['Country']=='Venezuela']['Population -2020'].nunique()!=1:
+         country_mistake.append(country)
 
+### country_mistake it is an empthy, so it is good
 
-# Data Analytics
+        
+    
+# Machine Learning Sentiment Analysis
 
-
+X_train=df['text']
+y=df['']
 
 
 
