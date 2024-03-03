80c2bdb8d6ce57300391aac764930168b2660473
Nevzat Taha <nevtahaayhan@gmail.com>
2024-03-03 21:30:18 +0300

    problem solved

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import re 
-from sklearn.metrics import accuracy_score,ConfusionMatrixDisplay,classification_report
 import joblib
 import streamlit as st
 import plotly.express as px
